0cd6b09037534c4d2b328d7df9456b96a4053d5a
linexjlin <line.xj.lin@gmail.com>
2023-08-04 14:15:54 +0800

    API works

---
+++ api.py
@@ -0,0 +1,37 @@
+import os
+import json
+from fastapi import FastAPI, File, UploadFile
+
+from doc_search import DocSearchManage
+
+scenes_path = "scenes"
+
+doc_manager = DocSearchManage()
+
+app = FastAPI()
+
+@app.post("/api/upload/{scene}/{path}")
+async def create_upload_file(scene: str, path: str, file: UploadFile = File(...)):
+    storage_path = f"{scenes_path}/{scene}/{path}"
+    user_path = f"{scene}/{path}"
+    os.makedirs(storage_path, exist_ok=True)  # 创建路径（如果不存在）
+
+    # 保存上传的文件
+    save_path = os.path.join(storage_path, file.filename)
+    with open(save_path, "wb") as f:
+        f.write(await file.read())
+        dcs = doc_manager.get(scene)
+        f.close()
+        dcs.load_to_db()
+
+    return {"message": f"File {file.filename} uploaded to {user_path}"}
+
+@app.get("/api/{scene}/search")
+async def search(scene: str,q: str):
+    dcs = doc_manager.get(scene)
+    results = dcs.query(q)
+    json_list = []
+    for result in results:
+        print(result)
+        json_list.append({"page_content": result[0].page_content, "source": result[0].metadata['source'],"socre":result[1]})
+    return json_list
---
+++ doc_search.py
@@ -0,0 +1,66 @@
+from langchain.embeddings.sentence_transformer import SentenceTransformerEmbeddings
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.vectorstores import Chroma
+from langchain.document_loaders import TextLoader
+from langchain.document_loaders import DirectoryLoader
+import os
+
+scenes_path = "scenes"
+index_path = "indexes"
+
+class DocSearch:
+    def __init__(self, model_name="all-MiniLM-L6-v2", data_path=scenes_path, persist_directory=".chroma"):
+        self.model_name = model_name
+        self.data_path = data_path
+        self.persist_directory = persist_directory
+        self.embedding_function = SentenceTransformerEmbeddings(model_name=self.model_name)
+        self.text_splitter = CharacterTextSplitter(chunk_size=800, chunk_overlap=0)
+        #self.loader = TextLoader(self.data_path)
+        self.db = None
+
+    def load_to_db(self):
+        self.loader = DirectoryLoader(self.data_path, glob="**/*.txt", loader_cls=TextLoader)
+        documents = self.loader.load()
+        print(f"reload documents from {self.data_path}")
+        # filter the document.page_content not empty document in documents
+        documents = [document for document in documents if document.page_content != ""]
+        if len(documents)>0:
+            docs = self.text_splitter.split_documents(documents)
+            self.db = Chroma.from_documents(docs, self.embedding_function, persist_directory=self.persist_directory)
+
+    def query(self, question):
+        if self.db is None:
+            #raise ValueError("Database not loaded. Call load_to_db() before querying.")
+            return []
+
+        docs = self.db.similarity_search_with_score(question)
+        return docs
+
+"""
+search = DocSearch(data_path="docs/md/eng.txt",persist_directory=".chroma3")
+search.load_to_db()
+
+question = "What is Faiss?"
+results = search.query(question)
+print(results)
+"""
+
+class DocSearchManage:
+    def __init__(self):
+        self.data = {}
+
+    def new(self, scene_id):
+        if scene_id not in self.data:
+            self.data[scene_id] = DocSearch()
+        return self.data[scene_id]
+
+    def get(self, scene_id):
+        if scene_id not in self.data:
+            path = f"{scenes_path}/{scene_id}"
+            os.makedirs(path, exist_ok=True)
+            data_path=f"{scenes_path}/{scene_id}"
+            print(data_path)
+            self.data[scene_id] = DocSearch(data_path=data_path, persist_directory=f"{index_path}/.chroma.{scene_id}")
+            self.data[scene_id].load_to_db()
+        return self.data[scene_id]
+    
